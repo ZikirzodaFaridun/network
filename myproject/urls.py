6ecc4396fd09a097dc174609db0557108d8d4dc0
zikirzodafaridun <f62710546@gmail.com>
2024-11-10 14:59:33 +0300

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
