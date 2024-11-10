efc55bcf75a0891fcc9e5e34a847f589f7a5b4a7
zikirzodafaridun <f62710546@gmail.com>
2024-11-10 10:36:16 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Nothing'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
