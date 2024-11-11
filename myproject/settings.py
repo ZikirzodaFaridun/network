733bcb544d9e3d9183db3bbe003faa5eeb2ffe3d
zikirzodafaridun <f62710546@gmail.com>
2024-11-11 14:39:35 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Nothing'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['network-two-flax.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
